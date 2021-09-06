c66bc4c1e48077e16ebf5665d3c9935c82bd86af
James Fraser <james.fraser@fanduel.com>
2021-09-06 12:58:12 +0100

    Initial commit

---
+++ src/main/java/ConfigManagement/ConfigManager.java
@@ -0,0 +1,25 @@
+package ConfigManagement;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigManager {
+    private static final Properties config;
+
+    static {
+        config = new Properties();
+        try {
+            config.load(new FileReader("src/main/resources/config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String imdbFileLocation() {
+        return config.getProperty("imdbFileLocation");
+    }
+    public static String outputFileLocation() {
+        return config.getProperty("outputFileLocation");
+    }
+}
